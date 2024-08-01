10b7b12371356427b7bb9e04f985f4dc66cba3aa
kanjitta <jittakan2539@gmail.com>
2024-08-01 15:19:25 +0700

    fix background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+		<main>
+			{/* <nav className="bg-white w-44">
+				<h1>sdsd</h1>
+			</nav> */}
+
+			<h1>Hello</h1>
+		</main>
 	);
 }
